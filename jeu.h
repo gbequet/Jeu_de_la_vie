4efc5f5ce5a875ddbca61b03d8bd6a316336d061
Guillaume <guillaume@MacBook-Air-de-Guillaume.local>
2018-03-12 13:54:52 +0100

    ajout calcul nb voisins non cyclique

--- jeu.h
+++ jeu.h
@@ -31,7 +31,19 @@ static inline int modulo(int i, int m){
  * \param g grille
  * \return le nombre de voisins de la cellule(i,j)
  */
-int compte_voisins_vivants (int i, int j, grille g);
+int compte_voisins_vivants_c(int i, int j, grille g);
+
+/**
+ * \fn compte_voisins_vivants (int i, int j, grille *g)
+ * \brief Compte le nombre de voisins d'une cellule(i,j)
+ *        sans les bords cycliques
+ * 
+ * \param i index de la ligne
+ * \param j index de la colonne
+ * \param g grille
+ * \return le nombre de voisins de la cellule(i,j)
+ */
+int compte_voisins_vivants_nc(int i, int j, grille g);
 
 /**
  * \fn evolue (grille *g, grille *gc)
@@ -41,6 +53,6 @@ int compte_voisins_vivants (int i, int j, grille g);
  * \param gc grille qui gardera les valeurs initiales de g
  * \return g a t+1
  */
-void evolue (grille *g, grille *gc);
+    void evolue(grille *g, grille *gc);
 
 #endif
